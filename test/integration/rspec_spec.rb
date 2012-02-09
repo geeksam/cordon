399338b1dbc0ae05d4ac2c0a067490234bfd06b5
Sam Livingston-Gray <geeksam@gmail.com>
2012-02-09 09:49:48 -0800

    Also, make spec test actually work

--- test/integration/rspec_spec.rb
+++ test/integration/rspec_spec.rb
@@ -1,8 +1,15 @@
 require File.join(File.dirname(__FILE__), *%w[.. helper])
 require 'rspec'
 
+Cordon.embargo :rspec
+
 describe "RSpec integration" do
   it 'works just fine if you adhere to the rules' do
     assert_that(42).should == 42
+  end
+
+  it 'explodes if you use standard^H^H evil RSpec idiom' do
+    naughty = lambda { 42.should == 6*9 }
+    assert_that(naughty).should raise_error(Cordon::ExpectationIncursion)
   end
 end
