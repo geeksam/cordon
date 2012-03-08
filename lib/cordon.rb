393355d2e5c55bc3834b8ea1a9a64f1b02810ba7
Sam Livingston-Gray <geeksam@gmail.com>
2012-03-08 10:18:08 -0800

    This approach should actually, y'know, work

--- lib/cordon.rb
+++ lib/cordon.rb
@@ -1,68 +1,72 @@
 module Cordon
-  ExpectationIncursion = Class.new(Exception)
+  CordonViolation = Class.new(Exception)
+  CordonedMethods = Hash.new { |hash, key| hash[key] = Hash.new }
+  OneTimePasses = []
+
+  module Sanitaire
+    def assert_that(predicate)
+      Cordon.admit_one(predicate)
+      return predicate
+    end
+
+    def __cordon__call_method__(subject, method, *args)
+      raise ::Cordon::CordonViolation unless Cordon.permitted?(self)
+      unbound_method = CordonedMethods[subject][method]
+      unbound_method.bind(self).call(*args)
+    end
+  end
 
   class << self
     def embargo(framework)
       case framework
       when :rspec
-        refuse :should, :should_not
       when :minitest_spec
         return unless defined?(MiniTest::Expectations)
         refuse *(MiniTest::Expectations.instance_methods - Module.instance_methods).map(&:to_s).sort
+        refuse Kernel, [:should, :should_not]
       else
         raise "I don't know how to embargo #{framework}!"
       end
     end
 
     # Declare specific methods as off-limits
-    def refuse(*methods)
-      methods.flatten.each do |method|
-        Sanitaire.refuse_method method
+    def refuse(subject, methods)
+      methods.each do |method|
+        next if CordonedMethods[subject][method]  # be idempotent
+
+        # Unbind the original method, and replace it with a wrapper that
+        # checks for permission before binding and calling the original
+        return unless unbind_method(subject, method.to_sym)
+        replace_method_with_permissions_checking_wrapper(subject, method.to_sym)
       end
     end
 
-    # Remove Cordon's protection for specific methods
-    def permit(*methods)
-      methods.flatten.each do |method|
-        Sanitaire.permit_method method
-      end
+    def admit_one(object)
+      OneTimePasses << object
     end
 
-    # Plumbing method; use this to apply Cordon's protection to a specific module or class.
-    def wrap_module(m)
-      m.module_eval do
-        include ::Cordon::Sanitaire
-      end
+    def permitted?(object)
+      !! OneTimePasses.delete(object)
     end
-  end
 
-  module Sanitaire
-    Whitelist = []
+  protected
 
-    class << self
-      def refuse_method(method)
+    def unbind_method(subject, method)
+      CordonedMethods[subject][method] = subject.instance_method(method)
+    end
+
+    def replace_method_with_permissions_checking_wrapper(subject, method)
+      __cordon__receiver__ = subject  # use a name unlikely to collide in the object's own binding
+      subject.instance_eval do
         define_method(method) do |*args|
-          return super(*args) if __cordon__expectations_allowed__
-          raise ::Cordon::ExpectationIncursion
+          __cordon__call_method__(__cordon__receiver__, method, *args)
         end
       end
-
-      def permit_method(method)
-        undef_method method if instance_methods.include?(method)
-      end
-    end
-
-    def __cordon__expectations_allowed__
-      !!Whitelist.delete(self)
-    end
-
-    def assert_that(predicate)
-      Whitelist << predicate
-      predicate
     end
   end
 end
 
-
 # Protect ALL the things!
-Cordon.wrap_module Object
+class Object
+  include Cordon::Sanitaire
+end
--- test/unit/cordon_unit_test.rb
+++ test/unit/cordon_unit_test.rb
@@ -1,49 +1,53 @@
 require File.join(File.dirname(__FILE__), 'unit_helper')
 
 module Kernel
-  ExpectationMethodCallReachedKernel = Class.new(Exception)
-  def should(*_);     raise ExpectationMethodCallReachedKernel; end
-  def should_not(*_); raise ExpectationMethodCallReachedKernel; end
+  VerbotenMethodCallReachedKernel = Class.new(Exception)
+  def verboten_method(*_)
+    raise VerbotenMethodCallReachedKernel
+  end
 end
 
 
 # By default, Cordon does nothing!
 class CordonDefaultBehaviorUnitTest < Test::Unit::TestCase
-  def test_allows_calls_to__should
-    assert_raises(ExpectationMethodCallReachedKernel) { Object.new.should }
-  end
-
-  def test_allows_calls_to__should_not
-    assert_raises(ExpectationMethodCallReachedKernel) { Object.new.should_not }
+  def test_allows_calls_to__verboten_method
+    assert_raises(VerbotenMethodCallReachedKernel) { Object.new.verboten_method }
   end
 end
 
 # You can tell Cordon to refuse certain methods on all objects.
 class CordonUnitTest < Test::Unit::TestCase
   def setup
-    Cordon.refuse :should, :should_not
-  end
-  def teardown
-    Cordon.permit :should, :should_not
+    Cordon.refuse Kernel, [:verboten_method]
   end
 
-  # Calling #should or #should_not on a random object will raise an exception...
-
-  def test_raises_exception_when_calling__should
-    assert_raise(Cordon::ExpectationIncursion) { Object.new.should }
+  def foo
+    @foo ||= Object.new
   end
 
-  def test_raises_exception_when_calling__should_not
-    assert_raise(Cordon::ExpectationIncursion) { Object.new.should_not }
+  # Calling #verboten_method on a random object will raise an exception...
+  def test_raises_exception_when_calling__verboten_method
+    assert_raise(Cordon::CordonViolation) do
+      foo.verboten_method
+    end
   end
 
   # ...unless you explicitly wrap that object in an #assert_that call
-  
-  def test_allows_explicit_calls_to__should
-    assert_raises(ExpectationMethodCallReachedKernel) { assert_that(Object.new).should }
+  def test_allows_explicit_calls_to__verboten_method
+    assert_raises(VerbotenMethodCallReachedKernel) do
+      assert_that(foo).verboten_method
+    end
   end
 
-  def test_allows_explicit_calls_to__should_not
-    assert_raises(ExpectationMethodCallReachedKernel) { assert_that(Object.new).should_not }
+  # ...every time!
+  def test_permission_provided_by_assert_that_only_works_once
+    assert_raise(Cordon::CordonViolation) do
+      begin
+        assert_that(foo).verboten_method
+      rescue VerbotenMethodCallReachedKernel
+        # carry on
+      end
+      foo.verboten_method
+    end
   end
 end
